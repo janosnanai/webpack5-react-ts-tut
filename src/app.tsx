687199e50006d648ea0bd62833cb97343fa9611b
Janos Nanai <janos.nanai@gmail.com>
2023-02-17 19:56:58 +0100

    webpack rules

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,15 @@
+import "./styles.css";
+import IMAGE from "./Rick-And-Morty-PNG-Images.png";
+import LOGO from "./vercel.svg";
+
 function App() {
-  return <h1>Hello, this is a ts react webpack v5 starter!</h1>;
+  return (
+    <>
+      <h1>Hello, this is a ts react webpack v5 starter!</h1>
+      <img src={IMAGE} alt="ram img" width={300} height={200} />
+      <img src={LOGO} alt="ram img" width={300} />
+    </>
+  );
 }
 
 export default App;
