9800e78e8ab167f99a305912e45fce4dd1356316
Janos Nanai <janos.nanai@gmail.com>
2023-02-17 20:23:51 +0100

    multiple envs

--- src/app.tsx
+++ src/app.tsx
@@ -2,10 +2,15 @@ import "./styles.css";
 import IMAGE from "./Rick-And-Morty-PNG-Images.png";
 import LOGO from "./vercel.svg";
 
+const MODE = process.env.NODE_ENV;
+const NAME = process.env.name;
+
 function App() {
   return (
     <>
-      <h1>Hello, this is a ts react webpack v5 starter!</h1>
+      <h1>
+        Hello, this is a ts react webpack v5 starter! {MODE} - {NAME}
+      </h1>
       <img src={IMAGE} alt="ram img" width={300} height={200} />
       <img src={LOGO} alt="ram img" width={300} />
     </>
